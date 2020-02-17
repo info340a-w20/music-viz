5b7905394e6bed3fab9cb6fdbfcec4dacd39bedb
Alex Wong <alwong34@uw.edu>
2020-02-16 21:24:29 -0800

    started javascript for main page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,100 @@
 'use strict';
 
 let state = {
+    currSong: { },
+    color: "",
+    shape : "",
+    width: 5,
+    volume: 50
+}
 
+window.onload = function() {
+  
+    var file = document.getElementById("thefile");
+    var audio = document.getElementById("audio");
+    
+    file.onchange = function() {
+      var files = this.files;
+      audio.src = URL.createObjectURL(files[0]);
+      audio.load();
+      audio.play();
+      var context = new AudioContext();
+      var src = context.createMediaElementSource(audio);
+      var analyser = context.createAnalyser();
+  
+      var canvas = document.getElementById("canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      var ctx = canvas.getContext("2d");
+  
+      src.connect(analyser);
+      analyser.connect(context.destination);
+  
+      analyser.fftSize = 256;
+  
+      var bufferLength = analyser.frequencyBinCount;
+      console.log(bufferLength);
+  
+      var dataArray = new Uint8Array(bufferLength);
+  
+      var WIDTH = canvas.width;
+      var HEIGHT = canvas.height;
+  
+      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+  
+      function renderFrame() {
+        requestAnimationFrame(renderFrame);
+  
+        x = 0;
+  
+        analyser.getByteFrequencyData(dataArray);
+  
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  
+        for (var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          
+          var r = barHeight + (25 * (i/bufferLength));
+          var g = 250 * (i/bufferLength);
+          var b = 50;
+  
+          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+          ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+  
+          x += barWidth + 1;
+        }
+      }
+  
+      audio.play();
+      renderFrame();
+    };
+  };
+
+class Song {
+    constructor(title, artist, prev, cover) {
+        this.title = title;
+        this.artist = artist;
+        this.prev = prev;
+        this.cover= cover;
+    }
+
+    render() {
+        let card = document.createElement("div");
+        card.classList.add("card", "m-2");
+        card.style.width = "18rem;";
+
+        let title = document.createElement("h1").innerHTML = this.title;
+        title.classList.add("ml-2");
+        card.appendChild(title);
+
+        let author = document.createElement("p").innerHTML = "by " + this.artist;
+        author.classList.add("song-author", "ml-3");
+        card.appendChild(author);
+
+        document.querySelector("#song-list").appendChild(card);
+    }
 }
 
