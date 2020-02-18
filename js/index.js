c4b7ba69b6c1a22809edcf4e3bec0dfa045bc699
Alex Wong <alwong34@uw.edu>
2020-02-18 14:35:51 -0800

    almost works

--- js/index.js
+++ js/index.js
@@ -11,7 +11,10 @@ let state = {
   app: {}
 }
 
-let promise = fetch('../musicData.json')
+
+
+window.onload = function () {
+  let promise = fetch('../musicData.json')
   .then(res => res.json())
   .then(data => {
     state.songList = data;
@@ -20,49 +23,13 @@ let promise = fetch('../musicData.json')
   })
   .catch(err => console.error(err));
 
-// let promise = fetch("")
-// let musicData = JSON.parse(data);
-// console.log(musicData);
-
-// var express = require('express')
-// var cors = require('cors')
-// var app = express()
-
-// app.use(cors())
-
-// app.get('/products/:id', function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
-
-// app.listen(80, function () {
-//   console.log('CORS-enabled web server listening on port 80')
-// })
-
-// const DEEZER_URL = "https://api.deezer.com";
-
-// const PORT = 3000
-
-
-// var request = new XMLHttpRequest();
-
-// request.open('GET', 'https://api.deezer.com/search?q=track:"good things"', true);
-
-// request.onload = function() {
-//     let data = JSON.parse(this.response);
-//     console.log(data);
-// }
-
-// request.send();
-
-
-window.onload = function () {
-
   // *Visualizer render and functionality code
   var file = document.getElementById("thefile");
   var audio = document.getElementById("audio");
+  // let a = new Audio("url...")
+  // let a = new Audio()
 
   file.onchange = function () {
-    console.log(this.files);
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
@@ -110,8 +77,7 @@ window.onload = function () {
 
         if (state.color == "Default" || state.color == "") {
           ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        }
-        else {// if (state.color != "") {
+        } else { // if (state.color != "") {
           ctx.fillStyle = state.color;
         }
         ctx.fillRect(x, HEIGHT - barHeight, barWidth * state.width, barHeight);
@@ -123,85 +89,121 @@ window.onload = function () {
     audio.play();
     renderFrame();
   };
-};
 
+  function renderCanvas(audio) {
+    audio.load();
+    audio.play();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
 
-// class Song {
-//   constructor(title, artist, prev, cover) {
-//     this.title = title;
-//     this.artist = artist;
-//     this.prev = prev;
-//     this.cover = cover;
-//   }
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
 
-//   render() {
-//     let card = document.createElement("div");
-//     card.classList.add("card", "m-2");
-//     card.style.width = "18rem;";
+    src.connect(analyser);
+    analyser.connect(context.destination);
 
-//     let title = document.createElement("h1").innerHTML = this.title;
-//     title.classList.add("ml-2");
-//     card.appendChild(title);
+    analyser.fftSize = 256;
 
-//     let author = document.createElement("p").innerHTML = "by " + this.artist;
-//     author.classList.add("song-author", "ml-3");
-//     card.appendChild(author);
+    var bufferLength = analyser.frequencyBinCount;
 
-//     document.querySelector("#song-list").appendChild(card);
-//   }
-// }
-// * add event listeners to the color input
-let colorsBtns = document.querySelectorAll(".colorPick");
-colorsBtns.forEach(function(btn) {
-  btn.addEventListener("click", function() {
-    state.color = this.innerHTML;  
-  })
-})
+    var dataArray = new Uint8Array(bufferLength);
 
-// * add event listenter to the width slider
-let widthSlider = document.querySelector("#widthSlider");
-widthSlider.addEventListener("input", function() {
-  state.width = this.value / 10;
-})
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
 
-class App {
-  constructor(parent, songList) {
-    this.parent = parent;
-    this.songList = songList;
-  }
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
 
-  render() {
-    // this.songList.data.forEach(song => {
-    //   console.log(song);
-    // });
-    for (let i = 0; i < 10; i++) {
-      let item = this.songList.data[i];
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+      x = 0;
 
-      let card = document.createElement("a");
-      card.classList.add("card", "m-2");
-      card.href = item.preview;
-      card.target = "_blank";
+      analyser.getByteFrequencyData(dataArray);
 
-      // let wrapper = document.createElement("div");
-      // wrapper.style.width = "10rem";
-      // wrapper.style.textOverflow = "ellipses";
-      // card.appendChild(wrapper);
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      let title = document.createElement("h4");
-      title.innerHTML = item.title;
-      title.classList.add("ml-2");
-      card.appendChild(title);
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i] * 2;
 
+        var r = barHeight + (25 * (i / bufferLength));
+        var g = 250 * (i / bufferLength);
+        var b = 50;
 
+        if (state.color == "Default" || state.color == "") {
+          ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        } else { // if (state.color != "") {
+          ctx.fillStyle = state.color;
+        }
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth * state.width, barHeight);
 
-      let author = document.createElement("p");
-      author.innerHTML = "by " + item.artist.name;
-      author.classList.add("song-author", "ml-3");
-      card.appendChild(author);
+        x += barWidth * state.width + 1;
+      }
+    }
 
-      document.querySelector("#song-list").appendChild(card);
+    audio.play();
+    renderFrame();
+  };
 
+  // * add event listeners to the color input
+  let colorsBtns = document.querySelectorAll(".colorPick");
+  colorsBtns.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      state.color = this.innerHTML;
+    })
+  });
 
+  // * add event listenter to the width slider
+  let widthSlider = document.querySelector("#widthSlider");
+  widthSlider.addEventListener("input", function () {
+    state.width = this.value / 10;
+  });
+
+  class App {
+    constructor(parent, songList) {
+      this.parent = parent;
+      this.songList = songList;
+    }
+  
+    render() {
+      // this.songList.data.forEach(song => {
+      //   console.log(song);
+      // });
+      for (let i = 0; i < 10; i++) {
+        let item = this.songList.data[i];
+  
+        let card = document.createElement("a");
+        card.classList.add("card", "m-2");
+        card.target = "_blank";
+        card.addEventListener("click", function() {
+          var audio = document.getElementById("audio");
+          audio.src = item.preview;
+          // renderCanvas(audio);
+        })
+  
+        // let wrapper = document.createElement("div");
+        // wrapper.style.width = "10rem";
+        // wrapper.style.textOverflow = "ellipses";
+        // card.appendChild(wrapper);
+  
+        let title = document.createElement("h4");
+        title.innerHTML = item.title;
+        title.classList.add("ml-2");
+        card.appendChild(title);
+  
+  
+  
+        let author = document.createElement("p");
+        author.innerHTML = "by " + item.artist.name;
+        author.classList.add("song-author", "ml-3");
+        card.appendChild(author);
+  
+        document.querySelector("#song-list").appendChild(card);
+      }
     }
   }
-}
+};
