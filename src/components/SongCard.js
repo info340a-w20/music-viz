16f9f22a0fc6065ed4e70bdb2ded0d81af0febf8
Alex <alwong34@uw.edu>
2020-03-01 17:06:24 -0800

    working on home page and card

---
+++ src/components/SongCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+
+
+export class SongCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        <Card>
+            <div>
+                <CardContent>
+                    <Typography>
+                        {this.props.title}
+                    </Typography>
+                    <Typography>
+                        {this.props.author}
+                    </Typography>
+                </CardContent>
+                <CardMedia image={this.props.image} title={this.props.title} />
+            </div>
+        </Card>
+    }
+}
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SongCard } from '../../components/SongCard';
+
+export class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
