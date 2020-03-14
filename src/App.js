7dc11b927f2855847c18f9060f13ba5b923c3690
GavinSree <sreesg@uw.edu>
2020-03-14 16:28:30 -0700

    still on database

--- src/App.js
+++ src/App.js
@@ -55,71 +55,35 @@ export class App extends React.Component {
           preview: ""
         }
       },
-      trending: [
-        {
-            id: 'trending'+ 0,
-            cover: "https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752",
-            songs: [
-              {name:'Good things', artist:'Gavin Koman', preview:'https://cdns-preview-8.dzcdn.net/stream/c-81af51bb89fd01fa5c65470b6b38597e-4.mp3', cover: 'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752'}, 
-              {name:'In the Midst', artist:'Tom Misch', preview: 'https://cdns-preview-3.dzcdn.net/stream/c-3b6d163c64ce90ddf249f755a7608f1b-2.mp3', cover: 'https://images-na.ssl-images-amazon.com/images/I/81UCBBsAXkL._SL1200_.jpg'}
-            ],
-            name: 'Trending 1'
-        },
-        {
-            id: 'trending'+ 1,
-            cover: "https://images-na.ssl-images-amazon.com/images/I/919WO8q-nnL._SL1500_.jpg",
-            songs: [
-              {name:'Good things', artist:'Gavin Koman', preview:'https://cdns-preview-8.dzcdn.net/stream/c-81af51bb89fd01fa5c65470b6b38597e-4.mp3', cover: 'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752'}, 
-              {name:'In the Midst', artist:'Tom Misch', preview: 'https://cdns-preview-3.dzcdn.net/stream/c-3b6d163c64ce90ddf249f755a7608f1b-2.mp3', cover: 'https://images-na.ssl-images-amazon.com/images/I/81UCBBsAXkL._SL1200_.jpg'}
-            ],
-            name: 'Trending 2'
-        },
-        {
-            id:'trending'+ 2,
-            cover: "https://f4.bcbits.com/img/a1266503095_10.jpg",
-            songs: [
-              {name:'Good things', artist:'Gavin Koman', preview:'https://cdns-preview-8.dzcdn.net/stream/c-81af51bb89fd01fa5c65470b6b38597e-4.mp3', cover: 'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752'}, 
-              {name:'In the Midst', artist:'Tom Misch', preview: 'https://cdns-preview-3.dzcdn.net/stream/c-3b6d163c64ce90ddf249f755a7608f1b-2.mp3', cover: 'https://images-na.ssl-images-amazon.com/images/I/81UCBBsAXkL._SL1200_.jpg'}
-            ],
-            name: 'Trending 3'
-        },
-        {
-            id: 'trending'+ 3,
-            cover: "https://i.pinimg.com/736x/9e/4f/b5/9e4fb5e55aaca75150fc308e8985a32c.jpg",
-            songs: [
-              {name:'Good things', artist:'Gavin Koman', preview:'https://cdns-preview-8.dzcdn.net/stream/c-81af51bb89fd01fa5c65470b6b38597e-4.mp3', cover: 'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752'}, 
-              {name:'In the Midst', artist:'Tom Misch', preview: 'https://cdns-preview-3.dzcdn.net/stream/c-3b6d163c64ce90ddf249f755a7608f1b-2.mp3', cover: 'https://images-na.ssl-images-amazon.com/images/I/81UCBBsAXkL._SL1200_.jpg'}
-            ],
-            name: 'Trending 4'
-        }
-    ],
+  
     playlists: [
         {
             id: 0,
             cover: "https://images-na.ssl-images-amazon.com/images/I/919WO8q-nnL._SL1500_.jpg",
-            songs: [
-              {id:0,name:'Good things', artist:'Gavin Koman', preview:'https://cdns-preview-8.dzcdn.net/stream/c-81af51bb89fd01fa5c65470b6b38597e-4.mp3', cover: 'https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/76887268_171396757567282_6574615474514919914_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=Rj9mqlsJ7D0AX-3fxwM&oh=eb1b7369e40fad4b76d705d33c1fc85f&oe=5ECFA752'}, 
-              {id:1, name:'In the Midst', artist:'Tom Misch', preview: 'https://cdns-preview-3.dzcdn.net/stream/c-3b6d163c64ce90ddf249f755a7608f1b-2.mp3', cover: 'https://images-na.ssl-images-amazon.com/images/I/81UCBBsAXkL._SL1200_.jpg'}],
-            name: "Time to Chill"
+            songs: [],
+            name: "Welcome: It's Time to Chill"
         }
     ],
     playlistId: 1
     }
 
     this.playlistElement = React.createRef();
+
+    // Create playlistRef to store the user and its playlist
+    this.playlistRef = firebase.database().ref("playlist")
   }
 
   componentDidMount() {
     this.authListener();
-    this.getUserData();
+    // this.getUserData();
   }
   
   componentDidUpdate(prevProps, prevState) {
     // check on previous state
     // only write when it's different with the new state
-    if (prevState !== this.state) {
-      this.writeUserData();
-    }
+    // if (prevState !== this.state) {
+    //   this.writeUserData();
+    // }
   }
 
   addPlaylist = (playlist) => {
@@ -140,9 +104,7 @@ export class App extends React.Component {
   addSong = (song, id) => {
     // console.log(playlist)
     let playlists = this.state.playlists;
-    console.log(id)
     let playlist = playlists[id]
-    console.log('hellll',playlist)
     playlist.songs.push(song)
   }
 
@@ -154,20 +116,32 @@ export class App extends React.Component {
   authListener() {
       firebase.auth().onAuthStateChanged(
         (user) => {
-          console.log(user);
+          
           if(user) {
             this.setState({user});
+            // console.log("here");
             localStorage.setItem('user', user.uid);
-            const userRef = localStorage.user
+            const userRef = this.playlistRef.child(user.uid);
             userRef.on("value", (snapshot) => {
-              this.setState({playlist})
+              this.setState({playlist: snapshot.val()})
             })
+
+            // userRef.on("value", (snapshot) => {
+            //   this.setState({playlist})
+            // })
           } else {
             this.setState({user:null});
+            // console.log("here");
             localStorage.removeItem('user')
           }
         }
       )
+  }
+
+  save = () => {
+    console.log( "hello",firebase.auth().currentUser)
+    // const playlistRef = this.playlistRef.child(firebase.auth().currentUser.uid);
+    // playlistRef.set({playlist: this.playlist})
   }
 
   
@@ -177,17 +151,17 @@ export class App extends React.Component {
   }
 
   writeUserData = () => {
-    let user = firebase.database().ref(localStorage.user).set(this.state)
+    let user = firebase.database().ref(localStorage.user)
     console.log('DATA SAVED');
   }
-  getUserData = () => {
-    let ref = firebase.database().ref(localStorage.user);
-    ref.on('value', snapshot => {
-      const state = snapshot.val();
-      this.setState(state);
-    });
-    console.log('DATA RETRIEVED');
-  }
+  // getUserData = () => {
+  //   let ref = firebase.database().ref(localStorage.user);
+  //   ref.on('value', snapshot => {
+  //     const state = snapshot.val();
+  //     this.setState(state);
+  //   });
+  //   console.log('DATA RETRIEVED');
+  // }
 
   render() {
     if (!this.state.user) {
@@ -199,7 +173,7 @@ export class App extends React.Component {
       );
     }
     // firebase.database().ref('/').set(this.state);
-    console.log('this',localStorage.user);
+    // console.log('this',localStorage.user);
 
     return (
       <Router>
@@ -216,7 +190,7 @@ export class App extends React.Component {
                   <HomePage currSong={this.state.currSong} setSong={this.selectSong.bind(this)}/>
                 </Route> */}
                 <Route exact path='/playlist' render={() => <PlayListPage playlistId={this.state.playlistId} ref={this.playlistElement} playlists={this.state.playlists} addPlaylist={this.addPlaylist} 
-                trending={this.state.trending} />} />
+                trending={this.state.trending} save={this.save}/>} />
                 <Route path='/playlist/:playlistId' render={(renderProps) => <MyPlayList addSong={this.addSong} playlists={this.state.playlists} {...renderProps} trending={this.state.trending} />}/>
                 <Route exact path='/about'>
                   <AboutPage/>
